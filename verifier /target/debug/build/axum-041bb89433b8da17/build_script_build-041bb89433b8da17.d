01a53a4648ae30afad7b44a25847af8f0d5b34b3
onyekachi11 <emegwaonyeka@gmail.com>
2024-09-04 16:36:17 +0100

    fixing render issue

--- verifier /target/debug/build/axum-041bb89433b8da17/build_script_build-041bb89433b8da17.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/Documents/supplychain-tracker/frontend/verifier/target/debug/build/axum-041bb89433b8da17/build_script_build-041bb89433b8da17: /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/axum-0.6.20/build.rs
-
-/Users/mac/Documents/supplychain-tracker/frontend/verifier/target/debug/build/axum-041bb89433b8da17/build_script_build-041bb89433b8da17.d: /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/axum-0.6.20/build.rs
-
-/Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/axum-0.6.20/build.rs:
--- verifier /target/debug/build/num-traits-c8b5f2ae247715cf/build_script_build-c8b5f2ae247715cf.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/Documents/supplychain-tracker/frontend/verifier/target/debug/build/num-traits-c8b5f2ae247715cf/build_script_build-c8b5f2ae247715cf: /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.15/build.rs
-
-/Users/mac/Documents/supplychain-tracker/frontend/verifier/target/debug/build/num-traits-c8b5f2ae247715cf/build_script_build-c8b5f2ae247715cf.d: /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.15/build.rs
-
-/Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.15/build.rs:
